20d59b4e20962b4fcb886206479805603994b884
강성지 <kangseongji@177-176-2-128.user3g.veloxzone.com.br>
2022-01-07 17:13:04 +0900

    클래스 리펙토링
    * 다양한 클래스의 기능을 리펙토링한다.

--- HouseUtils/src/main/java/com/kang/housrutils/policy/PurchaseBrokeragePolicy.java
+++ HouseUtils/src/main/java/com/kang/housrutils/policy/PurchaseBrokeragePolicy.java
@@ -1,43 +1,24 @@
 package com.kang.housrutils.policy;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 매매일 때 중계수수료를 계산해주는 클래스
  */
+@Getter
 public class PurchaseBrokeragePolicy implements BrokeragePolicy{
-    //인터페이스니 제거
-//    public Long calculate(Long price) {
-//        // TODO: 가격을 받아와서 중계수수료 계산
-//        //아래로직은 생성과 계산이 같이 있음 ..
-////        BrokerageRule rule;
-////        if(price < 50_000_000) {
-////            rule = new BrokerageRule(0.6, 250_000L);
-////        } else if (price < 200_000_000) {
-////            rule = new BrokerageRule(0.5, 800_000L);
-////        } else if (price < 600_000_000) {
-////            rule = new BrokerageRule(0.4, null);
-////        } else if(price < 900_000_000){
-////            rule = new BrokerageRule(0.5, null);
-////        } else {
-////            rule = new BrokerageRule(0.9, null);
-////        }
-//        BrokerageRule rule = createBrokerageRule(price);
-//        return rule.calcMaxBrokerage(price);
-//
-//    }
+    private final List<BrokerageRule> rules;
 
-    public BrokerageRule createBrokerageRule(Long price) {
-        BrokerageRule rule;
-        if(price < 50_000_000) {
-            rule = new BrokerageRule(0.6, 250_000L);
-        } else if (price < 200_000_000) {
-            rule = new BrokerageRule(0.5, 800_000L);
-        } else if (price < 600_000_000) {
-            rule = new BrokerageRule(0.4, null);
-        } else if(price < 900_000_000){
-            rule = new BrokerageRule(0.5, null);
-        } else {
-            rule = new BrokerageRule(0.9, null);
-        }
-        return rule;
+    public PurchaseBrokeragePolicy() {
+        rules = Arrays.asList(
+                new BrokerageRule(50_000_000L,0.6, 250_000L),
+                new BrokerageRule(200_000_000L,0.5, 800_000L),
+                new BrokerageRule(600_000_000L,0.4),
+                new BrokerageRule(900_000_000L,0.5),
+                new BrokerageRule(Long.MAX_VALUE,0.9)
+        );
     }
 }
